1d49a91098b9b9c58d920816649deacdbb445f52
niuhongbin <niuhongbin111@163.com>
2018-07-02 20:18:17 +0800

    添加方式二

--- app/src/main/java/com/test/niuhongbin/buttondemo2/MainActivity.java
+++ app/src/main/java/com/test/niuhongbin/buttondemo2/MainActivity.java
@@ -17,23 +17,50 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
+    private Button btn,btn01;
+    private MyOnClickListener listener = new MyOnClickListener();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn1);//根据资源id获取view视图
+        btn01 = findViewById(R.id.btn2);
 
+//        方式二 内部类的形式
+        btn.setOnClickListener(listener);
+        btn01.setOnClickListener(listener);
+
+
+
+        //方式一：匿名内部类的形式  给button按钮设置单击事件
 //        给button按钮设置单击事件监听
 
-        btn.setOnClickListener(new View.OnClickListener(){
-//            当前按钮被单击时回调的方法
-            @Override
-            public void onClick(View v){
-                Toast.makeText(MainActivity.this,"我被点击了",Toast.LENGTH_SHORT).show();
+//        btn.setOnClickListener(new View.OnClickListener(){
+////            当前按钮被单击时回调的方法
+//            @Override
+//            public void onClick(View v){
+//                Toast.makeText(MainActivity.this,"我被点击了！！！",Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
 
-            }
-        });
+
 
     }
+//    定义一个内部类 进行调用
+    class MyOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.btn1:
+                    Toast.makeText(MainActivity.this,"第一个按钮被点击了",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.btn2:
+                    Toast.makeText(MainActivity.this,"第二个按钮被点击了",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
 }
